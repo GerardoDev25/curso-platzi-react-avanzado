7c3613413b200b5b4482242987f5ef163641b367
Gerardo Miranda <gerardodev25@gmail.com>
2022-07-04 21:21:11 -0400

    build an deploy en vercel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Mi proyecto con React Avanzado!!!')
+import React from "react";
+import reactDom from "react-dom";
+
+reactDom.render(<h1>hola platzi sdsd</h1>, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+/** @type {import('webpack').Configuration} */
 module.exports = {
   output: {
-    filename: 'app.bundle.js'
+    filename: "app.bundle.js",
   },
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
-}
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
