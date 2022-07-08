551305c9e8c9370c143bb18cf3625eccbb53e8b2
Gerardo Miranda <gerardodev25@gmail.com>
2022-07-08 08:20:05 -0400

    working with the components

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,18 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Category from "../Category";
 import { Item, List } from "./style";
 
-import { categories } from "../../../api/db.json";
+const useCategoriesData = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    window
+      .fetch("https://petgram-server-gerardo-miranda.vercel.app/categories")
+      .then((resp) => resp.json())
+      .then((result) => {
+        setCategories(result);
+        setLoading(false);
+      });
+  }, [setCategories]);
+
+  return { categories, loading };
+};
 
 const ListOfCategory = () => {
+  const { categories, loading } = useCategoriesData();
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      const newSwohFixed = window.scrollY > 200;
+      showFixed !== newSwohFixed && setShowFixed(newSwohFixed);
+    };
+    document.addEventListener("scroll", onScroll);
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => {
+    return (
+      <List fixed={fixed}>
+        {loading ? (
+          <Item key={"loading"}>
+            <Category />
+          </Item>
+        ) : (
+          categories.map((category) => (
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>
+          ))
+        )}
+      </List>
+    );
+  };
+
   return (
-    <List>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
-    </List>
+    <>
+      {renderList()} {showFixed && renderList(true)}
+    </>
   );
 };
 
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,35 @@
-import React from "react";
-import { MdFavoriteBorder } from "react-icons/md";
-import { Button, Img, ImgWrapper } from "./style";
+import React, { useRef, useState } from "react";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+
+import { useNearScreen } from "../../hooks/useNearScreen";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+
+import { Article, Button, Img, ImgWrapper } from "./style";
 
 const DEFAULT_IMAGE =
   "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+
 const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, element] = useNearScreen();
+  const key = "like-" + id;
+  const [liked, setLiked] = useLocalStorage(key, false);
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size="32px"/> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size="32px" /> {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   );
 };
 ``;
